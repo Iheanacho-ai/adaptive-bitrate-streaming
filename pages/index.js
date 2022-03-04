a563fb512346f76d6c8b1519425f97a48ac07b31
Iheanacho Amarachi <amarachi2812@gmail.com>
2022-03-04 02:53:15 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import "cloudinary-video-player/dist/cld-video-player.min.js";
+import "cloudinary-video-player/dist/cld-video-player.min.css";
+import { Cloudinary } from "cloudinary-core";
+import { useEffect } from "react";
+
+const Home = () => {
+  const cld = new Cloudinary({ cloud_name: "amarachi-2812", secure: true});
+
+  const up_options = {
+    resource_type: "video", type: "upload",
+    eager: [
+      { streaming_profile: "hd", format: "m3u8" }, 
+    ] 
+  };
+  cloudinary.v2.uploader.explicit("videoplayback_1_pr2hzi", up_options, function(result) {console.log(result); } );
+
+  useEffect(() => {
+    const videoPlayer = cld.videoPlayer("video-player", {
+      controls: true
+    });
+    videoPlayer.source("videoplayback_1_pr2hzi");
+  },[]);
+  
+  return (
+    <div>
+      <video id="video-player" />
+    </div>
+  );
+};
+export default Home;
